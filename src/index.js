e393eda46badd150019b11e069d245b9fc98880f
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-03-07 14:52:41 +0100

    fix: add missing exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import * as git from "isomorphic-git";
 export { git };
-export { default as IsomorphicGitFs } from "./IsomorphicGitFs.js"
+export { default as IsomorphicGitFs } from "./IsomorphicGitFs.js";
+export { default as GitHistory } from "./GitHistory.js";
+export { default as resolvePath } from "./resolvePath.js";
